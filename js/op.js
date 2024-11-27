3faca1570a3af0615e6f94afd4a5bdabae9c8c73
briangonzales1996 <brianjose00@gmail.com>
2024-11-27 16:45:20 -0300

    funciones

--- js/op.js
+++ js/op.js
@@ -1,54 +1,130 @@
+"use strict" 
+//variables u objetos
+class Productos{
+    constructor(){
+        this.nombre = verificarNombre().toLowerCase(); 
+        this.precio = parseInt(verificarNumero("ingrese precio del producto"))+"$";
+        this.id = verificarId();
+        this.cantidad = parseInt(verificarNumero("ingrese la cantidad de productos"));
+    }
+}
 
-//funciones
-function dineroComprobarDato(dinero){
-    while(isNaN(dinero)||(dinero<0)){
-        dinero = prompt("ingrese un numero y mayor a 0");
+const variablesGlobales = {
+    opcion : "",
+    productos: []//array de lista de productos agregados como objeto
+}
+
+//FUNCIONES
+//verificamos si ingreso un nombre correcto
+const verificarNombre = ()=>{
+    let nombre = prompt("ingrese el nombre del producto");
+    while(!isNaN(parseInt(nombre)) || nombre === "" ){
+        nombre = prompt("El nombre no es valido, debe ser una palabra");
     }
-    return dinero;
+    return nombre 
 }
-function ingresarProducto(){
-    let nombre = prompt("ingrese el nombre del producto");   
-    let precio = comprobarPrecio();
-    alert("producto " + nombre +" se registro correctamente");
-    return precio;
-}
-function comprobarPrecio(){
-    let precio = parseInt(prompt ("ingrese precio del producto"));
-    while(isNaN(precio) || precio<0){
-        precio = parseInt(prompt("el dato debe ser numerico"));
+
+//verificamos si ingreso un numero
+const verificarNumero=(mensaje)=>{
+    let precio =prompt(mensaje);
+    while(isNaN(parseInt(precio))){
+        precio = prompt("ingrese un numero porfavor")
     }
-    return precio    
+    return precio
 }
-function comprobarTecla(finalizar){
-    while(finalizar != "s" && finalizar !="n"){
-        finalizar=prompt("ERROR.presione 's' para afirmar o 'n' para negar");
+
+//funcion bucle para buscar si se repite el id
+const buscarID = (produc,id)=>{
+    let idUsado = true;
+    for(let item in produc){
+        if(produc[item].id == id || isNaN(id)){
+            idUsado = false;
+        }
     }
-    return finalizar;
+    return idUsado
 }
-function comprobarCompra(dinero,precioTotal){
-    if(precioTotal>dinero){
-        alert("la compra no se puede realizar, verifique su cartera");
+
+function verificarId(){
+    let  id = parseInt(prompt("ingrese el id del producto"));
+    while(id || isNaN(id)){
+        if(variablesGlobales.productos.length !== 0){
+            let idUsado = buscarID(variablesGlobales.productos,id);
+            if(idUsado === false){
+                id = parseInt(prompt("ingrese otro id porque ya se encuentra registrado"));
+            }
+            else{return id}       
+        }
+        else{
+            return id
+        }
+    }
+    
+}
+
+function agregarProducto (){
+    const producto = new Productos();  
+    variablesGlobales.productos.push(producto);//ingrese los objetos creados en el array
+    alert("el producto " + producto.nombre + " se agrego correctamente")
+}
+
+function mostrarProductos(){
+    let lista = [];
+    //itineramos y agregamos productos a la lista
+    if(variablesGlobales.productos.length === 0){
+        alert("La lista de productos se encuentra vacia")
     }
     else{
-        dinero = dinero - precioTotal;
-        alert("la compra se realizo exitosamente, su vuelto es de : " + dinero + " pesos")
+        variablesGlobales.productos.forEach(element => {
+            lista.push('Producto: '+ element.nombre +"\n"+ "Precio: "+ element.precio + "\n" + 'Cantidad: ' + element.cantidad + '\n' + 'Id: ' + element.id + "\n-------------------");    
+        });
+        alert(lista.join('\n'))
     }
+    
 }
 
-//incio del programa
-document.addEventListener("DOMContentLoaded",function(){
-    let finalizar = "";
-    let precioTotal = 0;
-    let dinero = parseInt(prompt("ingrese dinero disponible"));
-    dinero = dineroComprobarDato(dinero);
-    do{
-        precioTotal += ingresarProducto();
-        finalizar=prompt("desea finalizar la compra? s/n");
-        finalizar=comprobarTecla(finalizar);
+const buscarProducto = ()=>{
+    let iden = parseInt(prompt("ingrese el id del producto"));
+    let resultado = variablesGlobales.productos.find(item=>item.id === iden);
+    if(resultado === undefined){
+        alert("no registraste ningun producto con ese identificador");
     }
-    while(finalizar != "s");
-    comprobarCompra(dinero,precioTotal);
-})
+    else {
+        alert('Producto: '+ resultado.nombre +"\n"+ "Precio: "+ resultado.precio + "\n" + 'Cantidad: ' + resultado.cantidad + '\n' + 'Id: ' + resultado.id)      
+    }    
+    
+}
+
+const eliminarProducto = ()=>{
+    let eliminar = prompt('ingrese numero de id ');
+    for(let item in variablesGlobales.productos){
+        if(variablesGlobales.productos[item].id === parseInt(eliminar)){
+            variablesGlobales.productos.splice(parseInt(item),1);
+            return 'el producto se elimino correctamente';
+        }
+    }
+    return "nose encontro el id, mire la 'lista de productos'"
+}
+
+//Inicio del programa
+do{
+    variablesGlobales.opcion = prompt("Este es su carrito\nSelecciona una de la siguientes opciones\n\n1-Agregar producto a la lista\n2-Eliminar producto de la lista\n3-Mostrar lista de productos agregados\n4-Buscar producto\n5-Salir");
+    variablesGlobales.opcion = parseInt(variablesGlobales.opcion)
+    switch(variablesGlobales.opcion){
+        case 1 : agregarProducto();break;
+        case 2 : { let productoEliminado = eliminarProducto()
+            alert(productoEliminado);
+            break;
+        }
+        case 3 : mostrarProductos();break;
+        case 4 : buscarProducto();break;
+        case 5 : alert("Gracias por utilizar nuestro servicio. Hasta luego");break;
+        default: alert('la opcion ingresada es incorrecta');break;
+    }
+
+}
+while(variablesGlobales.opcion != 5);
+
+
 
 
 
